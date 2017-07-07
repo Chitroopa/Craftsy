bcc5651994ab79b4c6bff752ef7ecf17ccc82a03
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-07-07 09:23:52 -0700

    Add user Authentication

--- db/migrate/20170707155555_devise_create_users.rb
+++ db/migrate/20170707155555_devise_create_users.rb
@@ -1,6 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |t|
+    change_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -31,7 +31,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+
     end
 
     add_index :users, :email,                unique: true
